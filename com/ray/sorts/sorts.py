f8780eff3a570f3fa2ccf536c7d05c7717131e86
marui <markmarui@vip.qq.com>
2019-07-12 00:00:46 +0800

    冒泡排序和顺序查找

--- com/ray/sorts/sorts.py
+++ com/ray/sorts/sorts.py
@@ -13,9 +13,37 @@ def select_sort(origin_items, comp=lambda x, y: x < y):
     return items
 
 
-def bubble_sort(origin_items,comp = lambda x,y:x<y):
-    item =  origin_items[:]
+def bubble_sort(origin_items, comp=lambda x, y: x < y):
+    item = origin_items[:]
+    for i in range(len(item) - 1):
+        temp = False
+        for j in range(i, len(item) - 1 - i):
+            if comp(item[j - 1], item[j]):
+                item[j], item[j - 1] = item[j - 1], item[j]
+                temp = True
+        if temp:
+            temp = False
+            for j in range(len(item) - 2 - i, i, -1):
+                if comp(item[j - 1], item[j]):
+                    item[j], item[j - 1] = item[j - 1], item[j]
+                    temp = True
+        if not temp:
+            break
+    return item
 
+
+def seq_search(items, key):
+    for i in range(len(items) - 1):
+        if items[i] == key:
+            return i
+    return -1
+
+
+def seq_find(items, key):
+    for index, item in enumerate(items):
+        if item == key:
+            return index
+    return -1
 
 
 if __name__ == '__main__':
