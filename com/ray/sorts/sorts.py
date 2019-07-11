0bddcd2f9d73fb261dc4203c80edbbdf3a74a939
marui <markmarui@vip.qq.com>
2019-07-12 00:14:12 +0800

    归并排序

--- com/ray/sorts/sorts.py
+++ com/ray/sorts/sorts.py
@@ -59,6 +59,30 @@ def bin_search(items, key):
     return -1
 
 
+def merge(items, comp=lambda x, y: x <= y):
+    if len(items) < 2:
+        return items[:]
+    mid = len(items) // 2
+    left = merge_sort(items[:mid], comp)
+    right = merge_sort(items[mid:], comp)
+    return merge(left, right, comp)
+
+
+def merge_sort(items1, items2, comp):
+    items = []
+    index1, index2 = 0, 0
+    while index1 < len(items1) and index2 < len(items2):
+        if comp(items1[index1], items2[index2]):
+            items.append(items1[index1])
+            index1 += 1
+        else:
+            items.append(items2[index2])
+            index2 += 1
+    items += items1[index1:]
+    items += items2[index2:]
+    return items
+
+
 if __name__ == '__main__':
     t1 = [2, 7, 6, 3, 5]
     print(select_sort(t1))
